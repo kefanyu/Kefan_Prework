e38a5294395bcdc8c52f1b9014563ebe643e7579
kefanyu <59634672+kefanyu@users.noreply.github.com>
2021-09-25 15:32:09 -0700

    My codepath prework
    write a swift code for tip calculator

---
+++ kefan_prework/swiftcode.swift
@@ -0,0 +1,8 @@
+//
+//  swiftcode.swift
+//  kefan_prework
+//
+//  Created by 凤凰院红脸 on 9/23/21.
+//
+
+import Foundation
---
+++ kefan_preworkTests/kefan_preworkTests.swift
@@ -0,0 +1,33 @@
+//
+//  kefan_preworkTests.swift
+//  kefan_preworkTests
+//
+//  Created by 凤凰院红脸 on 9/23/21.
+//
+
+import XCTest
+@testable import kefan_prework
+
+class kefan_preworkTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
