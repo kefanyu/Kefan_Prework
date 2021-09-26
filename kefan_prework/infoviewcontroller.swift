8a3bf5dbea53692cca752785eb1ad22e5448cb87
kefanyu <59634672+kefanyu@users.noreply.github.com>
2021-09-26 15:12:49 -0700

    Create infoviewcontroller.swift

---
+++ kefan_prework/infoviewcontroller.swift
@@ -0,0 +1,18 @@
+//
+//  infoviewcontroller.swift
+//  kefan_prework
+//
+//  Created by 凤凰院红脸 on 9/26/21.
+//
+
+import UIKit
+
+class infoviewcontroller: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Info Page"
+    }
+    
+}
+
